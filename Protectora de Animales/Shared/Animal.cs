1c5043520296e14d4abc790727f40b876a44a433
Ignacio Esteves <149106695+NachitoE@users.noreply.github.com>
2025-07-09 19:11:43 -0300

    creating classes

---
+++ Protectora de Animales/Shared/Animal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class Animal
+    {
+        #region Fields/Properties
+        public string ID
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+        private string _id;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        private string _name;
+        public string Species
+        {
+            get
+            {
+                return _species;
+            }
+            set
+            {
+                _species = value;
+            }
+        }
+        private string _species;
+        #endregion
+    }
+}
---
+++ Protectora de Animales/Shared/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class User
+    {
+        #region Fields/Properties
+        public string ID
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+        private string _id;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        private string _name;
+        public string DNI
+        {
+            get
+            {
+                return _dni;
+            }
+            set
+            {
+                _dni = value;
+            }
+        }
+        public string _dni;
+        #endregion
+    }
+}
