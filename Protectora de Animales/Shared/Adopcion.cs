55ea18fd59279ddb2d92d59d035e68501e7707b4
mimi <cami.stella094@gmail.com>
2025-07-10 15:34:56 -0300

    creando clases y cruds (provisional)

---
+++ Protectora de Animales/Shared/Adopcion.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    internal class Adopcion
+    {
+        #region Fields/Properties
+        private int m_IdAdopcion;
+        private List<Animal> m_animales;
+        private User m_usuario;
+        private DateTime m_fechaAdopcion;
+        private string m_estado;
+        private string m_descripcion;
+
+        public int IdAdopcion
+        {
+            get { return m_IdAdopcion; }
+            set { m_IdAdopcion = getNextId(); }
+        }
+        public List<Animal> Animales
+        {
+            get { return m_animales; }
+            set { m_animales = value; }
+        }
+        public User Usuario
+        {
+            get { return m_usuario; }
+            set { m_usuario = value; }
+        }
+
+        public DateTime FechaAdopcion
+        {
+            get { return m_fechaAdopcion; }
+            set { m_fechaAdopcion = value; }
+        }
+
+        public string Estado
+        {
+            get { return m_estado; }
+            set { m_estado = value; }
+        }
+
+        public string Descripcion
+        {
+            get { return m_descripcion; }
+            set { m_descripcion = value; }
+        }
+        private int getNextId()
+        {
+            //indice autoincremental, falta hacer
+            return 0;
+        }
+
+    
+    #endregion
+    #region Constructor
+    public Adopcion(int idAdopcion, List<Animal> animales, User usuario, DateTime fechaAdopcion, string estado, string descripcion)
+        {
+            m_IdAdopcion = idAdopcion;
+            m_animales = animales;
+            m_usuario = usuario;
+            m_fechaAdopcion = fechaAdopcion;
+            m_estado = estado;
+            m_descripcion = descripcion;
+        }
+        #endregion
+
+        #region CRUD
+        public void crearAdopcion()
+        {
+            // Después lo programo
+            Console.WriteLine("La adopción ha sido creada.");
+
+        }
+
+        public void mostrarAdopcion(Adopcion adopcion)
+        {
+            Console.WriteLine($"ID de adopción: {adopcion.IdAdopcion}");
+            Console.WriteLine($"Fecha de adopción: {adopcion.FechaAdopcion}");
+            Console.WriteLine($"Estado: {adopcion.Estado}");
+            Console.WriteLine($"Descripción: {adopcion.Descripcion}");
+            Console.WriteLine("Animales adoptados:");
+            foreach (var animal in adopcion.Animales)
+            {
+                Console.WriteLine($"- {animal.Name} ({animal.Species})");
+            }
+            Console.WriteLine($"Usuario: {adopcion.Usuario.Name} ({adopcion.Usuario.DNI})");
+        }
+
+        public void modificarAdopcion(Adopcion adopcion)
+        {
+            Console.WriteLine("Ingrese el nuevo estado de la adopción: /n nota: los estados posibles son: EN REVISION, ACEPTADA O RECHAZADA");
+            string nuevoEstado = Console.ReadLine();
+            adopcion.Estado = nuevoEstado;
+            Console.WriteLine("Ingrese una nueva descripción para la adopción:");
+            string nuevaDescripcion = Console.ReadLine();
+            adopcion.Descripcion = nuevaDescripcion;
+            Console.WriteLine("La adopción ha sido modificada.");
+            //preguntar a los chicos si hacemos las validaciones de scripts
+        }
+        public void eliminarAdopcion(Adopcion adopcion)
+        {
+            if (adopcion == null)
+            {
+                Console.WriteLine("No se puede eliminar una adopción nula.");
+                return;
+            }
+            else if (adopcion.Estado == "RECHAZADA")
+                //tendría que hacer un delete de la persistencia o del arreglo en memoria no? 
+                Console.WriteLine("La adopción ha sido eliminada exitosamente.");
+        }
+
+        #endregion
+    }
+}
---
+++ Protectora de Animales/Shared/Seguimiento.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    internal class Seguimiento
+    {
+        #region Fields/Properties
+        private int m_idSeguimiento;
+        private User m_usuario;
+        private DateTime m_fechaSeguimiento;
+        private string m_observacion;
+        private Animal m_animal;
+
+        public int IdSeguimiento
+        {
+            get { return m_idSeguimiento; }
+            set { m_idSeguimiento = value; }
+        }
+        public User Usuario
+        {
+            get { return m_usuario; }
+            set { m_usuario = value; }
+        }
+
+        public DateTime FechaSeguimiento
+        {
+            get { return m_fechaSeguimiento; }
+            set { m_fechaSeguimiento = value; }
+        }
+
+        public string Observacion
+        {
+            get { return m_observacion; }
+            set { m_observacion = value; }
+        }
+        #endregion
+
+        #region Constructor
+        public Seguimiento(int idSeguimiento, User usuario, DateTime fechaSeguimiento, string observacion, Animal animal)
+        {
+            m_idSeguimiento = idSeguimiento;
+            m_usuario = usuario;
+            m_fechaSeguimiento = fechaSeguimiento;
+            m_observacion = observacion;
+            m_animal = animal;
+        }
+        #endregion
+    
+
+    #region CRUD
+    public void crearSeguimiento()
+        {
+            int idSeguimiento = 0;
+            DateTime fechaSeguimiento = DateTime.Now;
+            Console.WriteLine("Ingrese la observación: ");
+            string observacion = Console.ReadLine();
+            User usuario = new User();
+            Animal animal = new Animal(); 
+            Seguimiento seguimiento = new Seguimiento(idSeguimiento,usuario,fechaSeguimiento, observacion, animal);
+            Console.WriteLine("El seguimiento ha sido creado.");
+
+        }
+        public void mostrarSeguimiento(Seguimiento seguimiento)
+        {
+            Console.WriteLine($"ID de seguimiento: {seguimiento.IdSeguimiento}");
+            Console.WriteLine($"Fecha de seguimiento: {seguimiento.FechaSeguimiento}");
+            Console.WriteLine($"Observación: {seguimiento.Observacion}");
+            Console.WriteLine($"Usuario: {seguimiento.Usuario.Name} ({seguimiento.Usuario.DNI})");
+           // Console.WriteLine($"Animal: {seguimiento.Animal.Name} ({seguimiento.Animal.Species})");
+
+
+        }
+        public void modificarSeguimiento(Seguimiento seguimiento)
+        {
+            Console.WriteLine("Ingrese la nueva observación: ");
+            string nuevaObservacion = Console.ReadLine();
+            seguimiento.Observacion = nuevaObservacion;
+            Console.WriteLine("El seguimiento ha sido modificado.");
+        }
+        public void eliminarSeguimiento(Seguimiento seguimiento)
+        {
+            //Que hago acá?
+            Console.WriteLine("El seguimiento ha sido eliminado.");
+        }
+    }
+    #endregion
+
+}
