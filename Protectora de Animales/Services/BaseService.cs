257279c036434c5b1670a26a8cca2bce357a6703
Ignacio Esteves <149106695+NachitoE@users.noreply.github.com>
2025-08-03 19:44:22 -0300

    Test: additional security for services reading

--- Protectora de Animales/Services/BaseService.cs
+++ Protectora de Animales/Services/BaseService.cs
@@ -11,12 +11,27 @@ namespace Services
     {
         protected abstract string _filePath { get; }
         private List<T> _list;
+        private DateTime _lastFileReadWriteTime;
+
         protected BaseService()
         {
             _list = Load();
+            _lastFileReadWriteTime = GetFileWriteTime();
         }
-        public virtual List<T> GetAll() => _list;
-        public virtual T GetByID(string id) => _list.First(x => x.Id == id);
+
+        public virtual List<T> GetAll()
+        {
+            var currentWriteTime = GetFileWriteTime();
+            if (currentWriteTime > _lastFileReadWriteTime)
+            {
+                _list = Load();
+                _lastFileReadWriteTime = currentWriteTime;
+            }
+            return _list;
+        }
+
+        public virtual T GetByID(string id) => GetAll().First(x => x.Id == id);
+
         public virtual void Save(T obj)
         {
             if (string.IsNullOrEmpty(obj.Id))
@@ -28,11 +43,13 @@ namespace Services
             _list.Add(obj);
             Persist();
         }
+
         public virtual void Delete(T obj)
         {
             _list.RemoveAll(x => x.Id == obj.Id);
             Persist();
         }
+
         protected virtual void Persist()
         {
             string location = GetFileLocation();
@@ -41,7 +58,9 @@ namespace Services
 
             var json = JsonSerializer.Serialize(_list, new JsonSerializerOptions { WriteIndented = true });
             File.WriteAllText(location, json);
+            _lastFileReadWriteTime = GetFileWriteTime();
         }
+
         protected virtual List<T> Load()
         {
             if (!File.Exists(GetFileLocation()))
@@ -49,15 +68,18 @@ namespace Services
 
             var json = File.ReadAllText(GetFileLocation());
             var deserializedList = JsonSerializer.Deserialize<List<T>>(json);
-            if (deserializedList == null)
-            {
-                return new List<T>();
-            }
-            return deserializedList;
+            return deserializedList ?? new List<T>();
         }
-        public bool IsEmpty() => _list.Count == 0;
+
+        public bool IsEmpty() => GetAll().Count == 0;
 
         private string GetFileDir() => Path.GetDirectoryName(GetFileLocation());
         private string GetFileLocation() => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "VeterinariaNet", _filePath);
+
+        private DateTime GetFileWriteTime()
+        {
+            var file = GetFileLocation();
+            return File.Exists(file) ? File.GetLastWriteTime(file) : DateTime.MinValue;
+        }
     }
 }
