c30c254a9b2cee81c7f0f8c4ad2622db2e8ddad6
Camila Stella <cami.stella094@gmail.com>
2025-09-01 10:57:29 -0300

    wip i'm modifying things

--- Protectora de Animales/WindowsFormsApp1/menuAdmin/Users/fm_ModifyUsers.cs
+++ Protectora de Animales/WindowsFormsApp1/menuAdmin/Users/fm_ModifyUsers.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Services;
 
 namespace WindowsFormsApp1.menuAdmin.Users
 {
@@ -15,6 +16,52 @@ namespace WindowsFormsApp1.menuAdmin.Users
         public fm_ModifyUsers()
         {
             InitializeComponent();
+            Load += ModifyUserFormLoad;
+        }
+
+        private void ModifyUserFormLoad(object sender, EventArgs e)
+        {
+            dgv_User.AutoGenerateColumns = false;
+            Shared.User WantedUser = UserService.Instance.GetByUserName(this.tb_getUsername.Text);
+            dgv_User.Columns.Add("UserName", "Nombre de Usuario");
+            dgv_User.Columns.Add("Name", "Nombre");
+            dgv_User.Columns.Add("SurName", "Apellido");
+            dgv_User.Columns.Add("DNI", "DNI");
+            dgv_User.Columns["UserName"].DataPropertyName = "UserName";
+            dgv_User.Columns["Name"].DataPropertyName = "Name";
+            dgv_User.Columns["SurName"].DataPropertyName = "SurName";
+            dgv_User.Columns["DNI"].DataPropertyName = "DNI";
+           
+
+            if (WantedUser != null)
+            {
+                dgv_User.DataSource = new List<Shared.User> { WantedUser };
+            }
+            else
+            {
+                dgv_User.DataSource = new List<Shared.User>();
+            }
+        }
+        private void llb_ShowAllUsers_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            fm_ShowAllUsers menuredir = new fm_ShowAllUsers();
+            menuredir.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_AcceptUsn_Click(object sender, EventArgs e)
+        {
+            Shared.User WantedUser = UserService.Instance.GetByUserName(this.tb_getUsername.Text);
+
+            if ((UserService.Instance.GetByUserName(this.tb_getUsername.Text) == null))
+            {
+                string message = "El usuario no existe";
+                string caption = "Error";
+                MessageBox.Show(message, caption);
+            }
+            
+            
         }
     }
 }
