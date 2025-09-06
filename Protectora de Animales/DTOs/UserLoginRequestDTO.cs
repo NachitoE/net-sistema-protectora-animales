b2316162c14ebcb696f29b30389f54d9bad00019
Ignacio Esteves <149106695+NachitoE@users.noreply.github.com>
2025-09-06 13:28:32 -0300

    .

--- Protectora de Animales/DTOs/UserLoginRequestDTO.cs
+++ Protectora de Animales/DTOs/UserLoginRequestDTO.cs
@@ -1,6 +1,6 @@
 namespace DTOs
 {
-    public class UserLoginDTO
+    public class UserLoginRequestDTO
     {
         public string UserName { get; set; }
         public string Password { get; set; }
