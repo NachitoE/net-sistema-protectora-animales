eb9d68769d53b7e7afed7af80b2c6f5e2c77de6c
Ignacio Esteves <149106695+NachitoE@users.noreply.github.com>
2025-09-10 11:47:35 -0300

    testing webapi

---
+++ Protectora de Animales 8/Protectora de Animales/WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController
+    {
+        [HttpGet(Name = "GetAllUsers")]
+        public IEnumerable<UserDTO> GetAll()
+        {
+            return new List<UserDTO>
+            {
+                new UserDTO
+                {
+                    Id = "1",
+                    Name = "John",
+                    SurName = "Doe",
+                },
+                new UserDTO
+                {
+                    Id = "2",
+                    Name = "Pepe",
+                    SurName = "Pepito",
+                }
+            };
+        }
+
+    }
+}
