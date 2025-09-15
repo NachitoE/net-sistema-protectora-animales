506a84551ccdd388d85765d2936d88493f7968b5
Ignacio Esteves <149106695+NachitoE@users.noreply.github.com>
2025-09-15 19:13:39 -0300

    added as comments prev methods in user service

--- Protectora de Animales 8/Protectora de Animales/Services/UsersService.cs
+++ Protectora de Animales 8/Protectora de Animales/Services/UsersService.cs
@@ -89,5 +89,53 @@ namespace Services
             */
             return new List<UserDTO>();
         }
+
+
+        /*
+         *  public bool IsValidUser(string userName, string password)
+        {
+            foreach (User user in GetAll())
+            {
+                if (user.UserName == userName && user.Password == password) return true;
+            }
+            return false;
+        }
+        public bool UserNameExists(string userName)
+        {
+            foreach (User user in GetAll())
+            {
+                if (user.UserName == userName) return true;
+            }
+            return false;
+        }
+
+        public static int GetRemainingCapacity(User user)
+        {
+            if (user.UserType == User.Type.Voluntario)
+            {
+                int userAnimalsCount = AnimalService.Instance.GetAll().Count(a => a.UserId == user.Id);
+                return User.MAX_CAPACITY_VOLUNTARIOS - userAnimalsCount;
+            }
+            else if (user.UserType == User.Type.Transito)
+            {
+                var house = HouseService.Instance.GetAll().FirstOrDefault(h => h.UserId == user.Id);
+                if (house == null)
+                    return 0; // No house found for the user
+
+                return house.Capacity -
+                    AnimalService.Instance.GetAll().Count(a => a.UserId == user.Id);
+            }
+            return 0;
+        }
+
+        public User GetByUserName(string userName)
+        {
+            return GetAll().FirstOrDefault(u => u.UserName == userName);
+        }
+        public void SetCurrentLoggedOnUser(User user)
+        {
+            _currentLoggedOnUser = user;
+        }
+        */
     }
 }
