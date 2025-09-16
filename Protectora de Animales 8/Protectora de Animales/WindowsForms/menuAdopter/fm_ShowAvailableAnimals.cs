296fcd2047a55690d168ee590245c769f82e0498
Ignacio Esteves <149106695+NachitoE@users.noreply.github.com>
2025-09-16 14:42:25 -0300

    changed entrypoint to showavailableanimals for exmaple

--- Protectora de Animales 8/Protectora de Animales/WindowsForms/menuAdopter/fm_ShowAvailableAnimals.cs
+++ Protectora de Animales 8/Protectora de Animales/WindowsForms/menuAdopter/fm_ShowAvailableAnimals.cs
@@ -1,30 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Data;
+using DTOs;
+using Helpers;
+using Infrastructure.API;
+using static Domain.Animal;
 
 namespace WindowsForms.menuAdopter
 {
     public partial class fm_ShowAvailableAnimals : Form
     {
+        private List<AnimalDTO> _availableAnimals;
         public fm_ShowAvailableAnimals()
         {
             InitializeComponent();
+            Load += ShowAvailableAnimalsFormLoadAsync;
+        }
+        private async void ShowAvailableAnimalsFormLoadAsync(object sender, EventArgs e)
+        {
+            SetupColumns();
+            dgv_animals.AutoGenerateColumns = false;
+            AnimalDTOClient animalClient = new AnimalDTOClient(new APIHttpClient());
+            List<AnimalDTO> availableAnimals = await animalClient.GetAllAvailableAnimalsAsync();
+            _availableAnimals = availableAnimals;
+            dgv_animals.DataSource = availableAnimals;
+        }
+        private void SetupColumns()
+        {
+            if (dgv_animals.Columns.Count > 0) return;
+
+            dgv_animals.AutoGenerateColumns = false;
+
+            dgv_animals.Columns.Add("Name", "Nombre");
+            dgv_animals.Columns.Add("Species", "Especie");
+            dgv_animals.Columns.Add("BirthDate", "Fecha de Nacimiento");
+            dgv_animals.Columns.Add("Description", "Descripción del animal");
+
+            dgv_animals.Columns["Name"].DataPropertyName = "Name";
+            dgv_animals.Columns["Species"].DataPropertyName = "Species";
+            dgv_animals.Columns["BirthDate"].DataPropertyName = "BirthDate";
+            dgv_animals.Columns["Description"].DataPropertyName = "Description";
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void btn_Dog_Click(object sender, EventArgs e)
+        {
+            if (!IsContentReady()) return;
+            FilterBySpecies(EnumConversion.SpeciesToString(SpeciesEn.Perro));
+        }
+
+        private void btn_Cat_Click(object sender, EventArgs e)
+        {
+            if (!IsContentReady()) return;
+            FilterBySpecies(EnumConversion.SpeciesToString(SpeciesEn.Gato));
+        }
+
+        private void btn_Bunny_Click(object sender, EventArgs e)
+        {
+            if (!IsContentReady()) return;
+            FilterBySpecies(EnumConversion.SpeciesToString(SpeciesEn.Conejo));
+        }
+
+        private void btn_Bird_Click(object sender, EventArgs e)
+        {
+            if (!IsContentReady()) return;
+            FilterBySpecies(EnumConversion.SpeciesToString(SpeciesEn.Pajaro));
+
+        }
+
+        private void dgv_animals_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void FilterBySpecies(string selSpecies)
         {
-
+            dgv_animals.AutoGenerateColumns = false;
+            var nonAdoptedBirds = _availableAnimals.Where(a => a.Species == selSpecies).ToList();
+            dgv_animals.DataSource = nonAdoptedBirds;
         }
+        private bool IsContentReady() => _availableAnimals != null;
     }
 }
--- Protectora de Animales/WindowsFormsApp1/Program.cs
+++ Protectora de Animales/WindowsFormsApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using WindowsFormsApp1.menuAdopter;
 
 namespace WindowsFormsApp1
 {
@@ -13,7 +14,7 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new fm_loggin());
+            Application.Run(new fm_ShowAvailableAnimals());
         }
     }
 }
