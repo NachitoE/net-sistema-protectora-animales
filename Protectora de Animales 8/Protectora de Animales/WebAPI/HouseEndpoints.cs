aa30ff0dc8375099b74c806ac22e505609802e9f
ThiagoK-03 <ignacioestevesz@gmail.com>
2025-09-25 09:45:34 -0300

    updated house endpoints

--- Protectora de Animales 8/Protectora de Animales/WebAPI/HouseEndpoints.cs
+++ Protectora de Animales 8/Protectora de Animales/WebAPI/HouseEndpoints.cs
@@ -7,6 +7,54 @@ namespace WebAPI
     {
         public static void MapHouseEndpoints(this WebApplication app)
         {
+            app.MapGet("/houses", () =>
+            {
+                HousesService housesService = new HousesService();
+                List<HouseDTO> houses = housesService.GetAll();
+                return Results.Ok(houses);
+            })
+                .WithName("Get All Houses")
+                .Produces<List<HouseDTO>>(StatusCodes.Status200OK)
+                .WithOpenApi();
+
+            app.MapGet("/houses/{id}", (string id) =>
+            {
+                try
+                {
+                    HousesService housesService = new HousesService();
+                    HouseDTO? houseDTO = housesService.Get(id);
+                    if (houseDTO == null) throw new ArgumentException("Casa no encontrada");
+                    return Results.Ok(houseDTO);
+                }
+                catch (ArgumentException ex)
+                {
+                    return Results.BadRequest(new { error = ex.Message });
+                }
+            })
+                .WithName("Get House By Id")
+                .Produces<HouseDTO>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status400BadRequest)
+                .WithOpenApi();
+
+            app.MapPut("/houses/capacity", (HouseChangeCapacityDTO houseCapDTO) =>
+            {
+                try
+                {
+                    HousesService housesService = new HousesService();
+                    HouseBaseResponseDTO houseResponseDTO = housesService.ChangeCapacity(houseCapDTO);
+                    if (!houseResponseDTO.Success) throw new ArgumentException(houseResponseDTO.Message);
+                    return Results.Ok(houseResponseDTO);
+                }
+                catch (ArgumentException ex)
+                {
+                    return Results.BadRequest(new { error = ex.Message });
+                }
+            })
+                .WithName("Update House Capacity")
+                .Produces<HouseDTO>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status400BadRequest)
+                .WithOpenApi();
+
             app.MapPost("/houses", (HouseRegisterRequestDTO houseRegDTO) =>
             {
                 try
