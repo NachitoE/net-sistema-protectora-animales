48cbb12c54dc0e3d1f008e7300afd9f805e7f124
Ignacio Esteves <149106695+NachitoE@users.noreply.github.com>
2025-10-01 12:17:09 -0300

    session storage jwt

---
+++ Protectora de Animales 8/Protectora de Animales/Blazor/BlazorServices/BlazorTokenStorage.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.JSInterop;
+
+namespace Blazor.BlazorServices
+{
+    public class BlazorTokenStorage
+    {
+        private IJSRuntime _js = null;
+        private const string TOKEN_ID = "authToken";
+        private const string SET_ITEM = "localStorage.setItem";
+        private const string GET_ITEM = "localStorage.getItem";
+        private const string REMOVE_ITEM = "localStorage.removeItem";
+        public BlazorTokenStorage(IJSRuntime js)
+        {
+            _js = js;
+        }
+        public async Task StoreToken(string token)
+        {
+            await _js.InvokeVoidAsync(SET_ITEM, TOKEN_ID, token);
+        }
+        public async Task<string?> GetToken()
+        {
+            return await _js.InvokeAsync<string?>(GET_ITEM, TOKEN_ID);
+        }
+        public async Task RemoveToken()
+        {
+            await _js.InvokeVoidAsync(REMOVE_ITEM, TOKEN_ID);
+        }
+    }
+}
