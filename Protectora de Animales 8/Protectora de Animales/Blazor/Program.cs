da4ddbe210105327725cd598f4e9cf5b762e4668
Ignacio Esteves <149106695+NachitoE@users.noreply.github.com>
2025-10-03 14:22:44 -0300

    mudblazor + new modify

--- Protectora de Animales 8/Protectora de Animales/Blazor/Program.cs
+++ Protectora de Animales 8/Protectora de Animales/Blazor/Program.cs
@@ -3,10 +3,12 @@ using Blazor.BlazorServices;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 // core session services
