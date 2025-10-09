d2ed4eaae03fd56b765b0e0e8ab3baaf09287ef2
ThiagoK-03 <149106695+NachitoE@users.noreply.github.com>
2025-10-09 09:39:25 -0300

    Create MedicalCheckUpDTOClient.cs

---
+++ Protectora de Animales 8/Protectora de Animales/Infrastructure.API/DTOs CRUDs/MedicalCheckUpDTOClient.cs
@@ -0,0 +1,22 @@
+﻿using DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.API.DTOs_CRUDs
+{
+    public class MedicalCheckUpDTOClient : BaseDTOClient<MedicalCheckUpDTO>
+    {
+        protected override string _endpoint => "medical-checkups";
+        public MedicalCheckUpDTOClient(IApiHttpClient apiHttpClient) : base(apiHttpClient)
+        {
+        }
+        public async Task<ApiResult<MedicalCheckUpDTO>> PostAsync(MedicalCheckUpRegisterDTO data)
+        {
+            var res = await _apiHttpClient.PostAsync<MedicalCheckUpDTO>($"{_endpoint}", data);
+            return res;
+        }
+    }
+}
