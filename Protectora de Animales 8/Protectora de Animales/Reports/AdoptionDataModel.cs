46d906853c434bcc16c032d4fa48a14c0213c5e0
Ignacio Esteves <149106695+NachitoE@users.noreply.github.com>
2025-10-17 21:34:58 -0300

    creating rpeort

---
+++ Protectora de Animales 8/Protectora de Animales/Reports/AdoptionDataModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Reports
+{
+    public class AdoptionDataModel
+    {
+        public string Header { get; set; }
+        public string Footer { get; set; }
+        public string Content { get; set; }
+        public List<AdoptionRowModel> Adoptions { get; set; }
+    }
+}
