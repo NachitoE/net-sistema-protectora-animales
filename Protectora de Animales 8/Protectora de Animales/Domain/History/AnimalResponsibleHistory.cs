6e1cb5245165537a14a54e68f6a8234000b0f91f
Ignacio Esteves <149106695+NachitoE@users.noreply.github.com>
2025-10-20 12:18:42 -0300

    animal responsible history

---
+++ Protectora de Animales 8/Protectora de Animales/Domain/History/AnimalResponsibleHistory.cs
@@ -0,0 +1,42 @@
+﻿namespace Domain.History
+{
+    public class AnimalResponsibleHistory
+    {
+        public string Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+        private string _id;
+        public DateTime AssignedDate
+        {
+            get => _assignedDate;
+            set => _assignedDate = value;
+        }
+        private DateTime _assignedDate;
+        public string ResponsibleId
+        {
+            get => _responsibleId;
+            set => _responsibleId = value;
+        }
+        private string _responsibleId;
+        public User Responsible
+        {
+            get => _responsible;
+            set => _responsible = value;
+        }
+        private User _responsible;
+        public string AnimalId
+        {
+            get => _animalId;
+            set => _animalId = value;
+        }
+        private string _animalId;
+        public Animal Animal
+        {
+            get => _animal;
+            set => _animal = value;
+        }
+        private Animal _animal;
+    }
+}
---
+++ Protectora de Animales 8/Protectora de Animales/Infrastructure.Data/AnimalResponsibleHistoryRepository.cs
@@ -0,0 +1,67 @@
+﻿using Domain;
+using Domain.History;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class AnimalResponsibleHistoryRepository
+    {
+        public DBContext CreateContext()
+        {
+            return new DBContext();
+        }
+
+        public void Add(AnimalResponsibleHistory a)
+        {
+            DBContext context = CreateContext();
+            context.AnimalResponsibleHistories.Add(a);
+            context.SaveChanges();
+
+        }
+
+        public AnimalResponsibleHistory? Get(string id)
+        {
+            DBContext context = CreateContext();
+            AnimalResponsibleHistory? foundAnimalRH = context.AnimalResponsibleHistories.Find(id);
+            return foundAnimalRH;
+        }
+
+        public IEnumerable<AnimalResponsibleHistory> GetAll()
+        {
+            DBContext context = CreateContext();
+            return context.AnimalResponsibleHistories;
+        }
+
+        public bool Update(AnimalResponsibleHistory a)
+        {
+            DBContext context = this.CreateContext();
+            AnimalResponsibleHistory? existingAnimalRH = context.AnimalResponsibleHistories.Find(a.Id);
+            if (existingAnimalRH != null)
+            {
+                existingAnimalRH.ResponsibleId = a.ResponsibleId;
+                existingAnimalRH.AnimalId = a.AnimalId;
+                existingAnimalRH.AssignedDate = a.AssignedDate;
+                context.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public bool Delete(string id)
+        {
+            DBContext context = this.CreateContext();
+            AnimalResponsibleHistory? existingAnimal = context.AnimalResponsibleHistories.Find(id);
+            if (existingAnimal != null)
+            {
+                context.AnimalResponsibleHistories.Remove(existingAnimal);
+                context.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+    }
+}
