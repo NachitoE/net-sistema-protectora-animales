3ed1be30fb70ec32d904db179bd6b983f042e6ed
Camila Stella <146795383+mimistella@users.noreply.github.com>
2025-10-21 05:38:24 -0300

    feat terminé todo lo de adopter no)?
    solo tengo que modificar el filtro de Available animals cuando nacho agregue el estado cuidado

--- Protectora de Animales 8/Protectora de Animales/WindowsForms/menuAdopter/fm_ShowAdoptionProgress.Designer.cs
+++ Protectora de Animales 8/Protectora de Animales/WindowsForms/menuAdopter/fm_ShowAdoptionProgress.Designer.cs
@@ -28,35 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            pictureBox1 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            dgv_UsersAdoption = new DataGridView();
+            lb_MyAdoptions = new Label();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)dgv_UsersAdoption).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
-            // pictureBox1
+            // dgv_UsersAdoption
             // 
-            pictureBox1.BackgroundImage = Properties.Resources.en_construcción__1_1;
-            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
-            pictureBox1.Location = new Point(201, 12);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(412, 352);
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
-            pictureBox1.Click += pictureBox1_Click;
+            dgv_UsersAdoption.BackgroundColor = SystemColors.Control;
+            dgv_UsersAdoption.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgv_UsersAdoption.Location = new Point(82, 115);
+            dgv_UsersAdoption.Name = "dgv_UsersAdoption";
+            dgv_UsersAdoption.Size = new Size(652, 342);
+            dgv_UsersAdoption.TabIndex = 0;
             // 
-            // fm_AdopterRequisites
+            // lb_MyAdoptions
+            // 
+            lb_MyAdoptions.AutoSize = true;
+            lb_MyAdoptions.BackColor = Color.OliveDrab;
+            lb_MyAdoptions.Font = new Font("Yu Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lb_MyAdoptions.ForeColor = SystemColors.ButtonHighlight;
+            lb_MyAdoptions.Location = new Point(335, 87);
+            lb_MyAdoptions.Name = "lb_MyAdoptions";
+            lb_MyAdoptions.Size = new Size(152, 25);
+            lb_MyAdoptions.TabIndex = 1;
+            lb_MyAdoptions.Text = "Mis Adopciones";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.OliveDrab;
+            pictureBox2.Location = new Point(57, 76);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(705, 409);
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // fm_ShowAdoptionProgress
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(pictureBox1);
-            Name = "fm_AdopterRequisites";
-            Text = "fm_AdopterRequisites";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ClientSize = new Size(827, 550);
+            Controls.Add(lb_MyAdoptions);
+            Controls.Add(dgv_UsersAdoption);
+            Controls.Add(pictureBox2);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "fm_ShowAdoptionProgress";
+            Text = "fm_AdoptionFollowing";
+            ((System.ComponentModel.ISupportInitialize)dgv_UsersAdoption).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox pictureBox1;
+        private DataGridView dgv_UsersAdoption;
+        private Label lb_MyAdoptions;
+        private PictureBox pictureBox2;
     }
 }
