0b45f0ff2232ce2feb441d673502214dd21ebc62
Ignacio Esteves <149106695+NachitoE@users.noreply.github.com>
2025-10-27 17:39:50 -0300

    deleted old project

--- Protectora de Animales/DTOs/AdoptionRequestDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace DTOs
-{
-    public class AdoptionRequestDTO
-    {
-        public string AnimalId { get; set; }
-        public string UserId { get; set; }
-        public string Comments { get; set; }
-    }
-}
--- Protectora de Animales/DTOs/UserLoginRequestDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace DTOs
-{
-    public class UserLoginRequestDTO
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
